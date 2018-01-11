a5007de4a3d6096077215a89e6c4afb10fc57750
TitouanT <titouan.teyssier@gmail.com>
2018-01-11 17:06:34 +0100

    code plus propre et generalisation à une grille

--- premices/autoFill.rb
+++ premices/autoFill.rb
@@ -1,10 +1,62 @@
-def autoFill(infos, nCase, debut=0)
-	lenPack = infos.sum + infos.size-1
-	nb_not_sure = nCase-lenPack
-	sol = info.map {|v|
-		sure=[v-nb_not_sure,0].max;
-		'.'*(v-sure) + '#'*sure
-	}*'.' + '.'*(nCase-lenPack)
+# recherche des cases que l'on peut noircire dès les début
+# 	1.pour une machine
+def getSureIndex(blocks, nCell)
+	lenPack = blocks.sum + blocks.size-1
+	nb_undefined = nCell - lenPack
 
+	sureIndex = []
+	firstCell = 0
+	blocks.each{|v|
+		(0...v).each{|blockCell|
+			sureIndex << firstCell + blockCell unless blockCell < nb_undefined
+		}
+		firstCell += v+1
+	}
+	return sureIndex
+end
+
+# 	2.pour un humain
+def getSureIndexHuman(blocks, nCell)
+	sureIndex = getSureIndex(blocks, nCell)
+
+	sol = '.'*nCell
+	indexes.each{|i| sol[i] = '#'}
 	sol.chars.each_slice(5).map(&:join).join('|')
 end
+
+
+
+# pour avoir toutes les cases que l'on peut noircire sans réfléchir sur toute une grille
+def startInfo(rows, cols)
+
+	lines = rows.map{|row| getSureIndex(row, cols.size)}
+	cols.each_with_index{|col, j|
+		sureIndex = getSureIndex(col, rows.size)
+		sureIndex.each{|i| lines[i] << j}
+	}
+
+	return lines
+end
+
+def startInfoHuman(rows, cols)
+	lines = startInfo(rows, cols)
+
+	lines.map!{|line|
+		sol = '.' * cols.size
+		line.each{|index| sol[index] = '#'}
+		sol.chars.each_slice(5).map(&:join).join('|')
+	}
+
+	lines*"\n"
+end
+
+
+
+# exemple 1 de la feuille de jacoboni
+rows = [[3], [1], [3], [2,1], [1,2]]
+cols = [[1], [3], [1, 2], [2,1], [1,2]]
+g = startInfo(rows, cols)
+puts g.to_s
+
+g = startInfoHuman(rows, cols)
+puts g
