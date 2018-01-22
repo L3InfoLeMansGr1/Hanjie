6a8386fa5717510efdca3049bca6d44c1d0b11ce
Marvin <marvinmoisy@gmail.com>
2018-01-22 22:54:03 +0100

    generation grille de booleen a partir d'image

--- premices/readByPixel.rb
+++ premices/readByPixel.rb
@@ -1,8 +1,32 @@
 require 'rmagick'
 include Magick
 
-cat = ImageList.new("apple.bmp")
-cat.each_pixel{ |i|
-  puts i
-}
-exit
+
+
+
+def generateGrid(imageName)
+	image = ImageList.new(imageName)
+	pos = 0
+	columns = image.columns
+	grid = Array.new()
+	line = Array.new()
+	image.each_pixel{ |pixel|
+		line.push(pixel.isBlack?)
+		pos+=1
+		if(pos==columns)
+			grid.push(line)
+			line = Array.new
+			pos=0
+		end
+	}
+	return grid
+end
+
+class Magick::Pixel
+	def isBlack?()
+		(self.green==0 && self.red ==0 && self.blue == 0 )? true : false
+	end
+end
+
+grid = generateGrid("apple.bmp")
+puts grid.last.size
