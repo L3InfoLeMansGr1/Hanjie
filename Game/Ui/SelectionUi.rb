264147acc0355682b8d41cbf9249a726faf22e3a
TitouanT <titouan.teyssier@gmail.com>
2018-02-20 17:36:23 +0100

    cleaning

--- Game/Ui/SelectionUi.rb
+++ Game/Ui/SelectionUi.rb
@@ -26,28 +26,14 @@ class SelectionUi
 	def unselect(arr)
 		arr.each { |cell|
 			@modified << cell
-			# # widget.override_background_color(nil, @normalColor)
-			# widget.override_background_color(nil, @normalColor)
-			# # widget.style.apply_default_background
-
-			# coreCell = cell.coreCell
-			# @assets.cell_asset(coreCell.state).applyOn(cell.gtkButton)
 			cell.unselect
-
 		}
 	end
 
 	def select(arr)
 		arr.each { |cell|
 			@modified << cell
-			# # widget.modify_bg(Gtk::StateType::NORMAL, @selectedColor)
-			# # widget.pry
-			# widget.override_background_color(nil, @selectedColor)
-
-			# coreCell = cell.coreCell
-			# @assets.cell_asset_selected(coreCell.state).applyOn(cell.gtkButton)
 			cell.select
-
 		}
 	end
 
