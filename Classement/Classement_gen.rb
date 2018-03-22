d53d3f5457c65f658f2a3ca7ced2d4891e95546e
thomas larmignat <l3info012@ic2s124-04.univ-lemans.fr>
2018-03-22 15:25:13 +0100

    singleton + ajoutable?

--- Classement/Classement_gen.rb
+++ Classement/Classement_gen.rb
@@ -1,57 +1,86 @@
 require 'yaml'
 require File.dirname(__FILE__) + "/Joueur_score"
 class Classement_gen
+	
+        @@instance = Classement_gen.new
+	private_class_method :new
 
-
-
-	def initialize()
-		@tabJoueurs  = Array.new()
+	def self.instance
+	    return @@instance
 	end
 
 
-	def bubble_sort
-		return @tabJoueurs if @tabJoueurs.size <= 1
+	def deserealiseJoueurs()
+		tab = Array.new
+		parsed = begin
+  		tab = YAML.load(File.open("./Classement/output.yml"))
+		rescue ArgumentError => e
+  			puts "Could not parse YAML: #{e.message}"
+		end
+		return tab if tab.size <= 1
+
 		swapped = true
 		while swapped do
 			swapped = false
-			0.upto(@tabJoueurs.size-2) do |i|
-				if @tabJoueurs[i].donneScore < @tabJoueurs[i+1].donneScore
-					@tabJoueurs[i], @tabJoueurs[i+1] = @tabJoueurs[i+1], @tabJoueurs[i]
+			0.upto(tab.size-2) do |i|
+				if tab[i].donneScore < tab[i+1].donneScore
+					tab[i], tab[i+1] = tab[i+1], tab[i]
 					swapped = true
 				end
 			end
 		end
-	  	@tabJoueurs
+	  	tab
 	end
 
 
 
 
- 	def afficheToi
- 		print(@tabJoueurs)
- 	end
-
-
-
-	def deserealiseJoueurs()
-		tab2 = Array.new
-		parsed = begin
-  		tab2 = YAML.load(File.open("./Classement/output.yml"))
-		rescue ArgumentError => e
-  			puts "Could not parse YAML: #{e.message}"
+	def ajoutable?(unJoueur)
+		res = Array.new
+		tab = deserealiseJoueurs
+		unMode = unJoueur.donneMode
+		tab.each do |joueur|
+			if joueur.donneMode == unMode
+				res.push(joueur)
+			end
+			
 		end
-		return(tab2)
+
+		if res.size < 10
+			return true
+		else
+			if  res[res.size-1].donneScore < unJoueur.donneScore
+				return true
+			else
+				return false 
+			end
+		end
+		
 	end
 
 
-	def getTab()
-		return(@tabJoueurs)
+	def remplaceDernier(tab, unJoueur)
+		res = Array.new
+		tab.each do |joueur|
+			if joueur.donneMode == unJoueur.donneMode
+				res.push(joueur)
+			end
+			
+		end
+		if(res.size < 10)
+			tab.push(unJoueur)
+		else
+		  	tab[tab.index(res[res.size-1])] = unJoueur
+		end
+		return tab
 	end
-
 
 	def ajouteJoueur(unJoueur)
-		@tabJoueurs.push(unJoueur)
-		File.open("output.yml", "w") {|f| f.write(@tabJoueurs.to_yaml) }
+		tab = deserealiseJoueurs
+		if ajoutable?(unJoueur)
+			tab = remplaceDernier(tab, unJoueur)
+			File.open("./Classement/output.yml", "w") {|f| f.write(tab.to_yaml) }
+		end	
 	end
 
 end
