ec6593cab8ffa7966cc5f2436e55e6689ca98d72
tarik yildiz <l3info037@ic2s114-18.univ-lemans.fr>
2018-04-06 16:22:16 +0200

    modifications sauvegardes

--- Sauvegardes/SauvegardeUi.rb
+++ Sauvegardes/SauvegardeUi.rb
@@ -11,48 +11,50 @@ class SauvegardeUi
 	attr_reader :gtkObject
 
 	def initialize(parent)
-		@gtkObject = Gtk::Box.new(:vertical,0)
+		 @gtkObject = Gtk::Box.new :vertical
 
-		box2 = Gtk::Box.new(:vertical, 5)
-		box2.border_width = 10
-		@gtkObject.pack_start(box2, :expand => true, :fill => true, :padding => 0)
-
-		scrolled_win = Gtk::ScrolledWindow.new
-		scrolled_win.set_policy(Gtk::PolicyType::AUTOMATIC,Gtk::PolicyType::AUTOMATIC)
-		box2.pack_start(scrolled_win,:expand => true, :fill => true, :padding => 0)
-
-
+		model = Gtk::ListStore.new(String)
+		
+		treeview = Gtk::TreeView.new(model)
+		setup_tree_view(treeview)
 
 		save = Sauvegardes.new("./Game/Core/Saves/","*.yml")
 		data = save.chargerRepertoire
-
-
-		model = Gtk::ListStore.new(String)
-		column = Gtk::TreeViewColumn.new("Liste des Sauvegardes", Gtk::CellRendererText.new, {:text => 0})
-
-
-		treeview = Gtk::TreeView.new(model)
-		treeview.append_column(column)
-		treeview.selection.set_mode(Gtk::SelectionMode::SINGLE)
-		scrolled_win.add_with_viewport(treeview)
-
-		data.each do |v|
+		data.each_with_index do |v|
 		  iter = model.append
-		  iter[0] =  v
+		  model.set_value(iter, 0, v)
 		end
+
+		box2 = Gtk::Box.new(:vertical, 10)
+		box2.border_width = 10		
+		@gtkObject.pack_start(box2, :expand => true, :fill => true, :padding => 0)
+
+		scrolled_win = Gtk::ScrolledWindow.new
+		scrolled_win.add_with_viewport(treeview)
+		scrolled_win.set_policy(:automatic,:automatic)
+		box2.pack_start(scrolled_win,:expand => true, :fill => true, :padding => 0)
+		
 
 		button = Gtk::Button.new(:label => "Charger")
 		#button.set_flags(Gtk::Widget::CAN_FOCUS)
 
 		button.signal_connect("clicked") do
-		  iter = treeview.selection.selected
-		  index = save.getIndex(model.get_value(iter,0)) #recuperation index
-			infos = save.getInfos(index)
-			parent.changeBackground("ecranDeJeu")
-			if infos[0] == "Ranked"
-				parent.display(RankedMode.new(nil,infos.join("&")))
-			elsif infos[0] == "TimeTrial"
-				parent.display(TimeTrialMode.new(infos.join("&")))
+			iter = treeview.selection.selected
+			if(iter != nil)
+				index = save.getIndex(model.get_value(iter,0)) #recuperation index
+				infos = save.getInfos(index)
+				parent.changeBackground("ecranDeJeu")
+				if infos[0] == "Ranked"
+					parent.display(RankedMode.new(nil,infos.join("&")))
+				elsif infos[0] == "TimeTrial"
+					parent.display(TimeTrialMode.new(infos.join("&")))
+			end			
+			else
+				dialog = Gtk::Dialog.new("Message",$main_application_window,Gtk::DialogFlags::DESTROY_WITH_PARENT,[ Gtk::Stock::OK, Gtk::ResponseType::NONE ])
+				dialog.signal_connect('response') { dialog.close }
+				dialog.child.add(Gtk::Label.new("Veuillez selectionner une sauvegarde svp"))	
+				dialog.show_all
+			
 			end
 		end
 
@@ -78,5 +80,12 @@ class SauvegardeUi
 			parent.display(parent.mainMenu)
 		})
 		box2.add(bRetour.gtkObject)
+	end
+
+	def setup_tree_view(treeview)
+			renderer = Gtk::CellRendererText.new
+			column = Gtk::TreeViewColumn.new("Liste des Sauvegardes", renderer,  :text => 0)
+			treeview.append_column(column)
+			
 	end
 end
