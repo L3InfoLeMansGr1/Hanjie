a7f09322ee1a849ab965d05c5d6df7d8e24a991e
thomas larmignat <l3info012@ic2s124-01.univ-lemans.fr>
2018-04-10 16:15:56 +0200

    Débug classement

--- Classement/Classement_gen.rb
+++ Classement/Classement_gen.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 require File.dirname(__FILE__) + "/Joueur_score"
 class Classement_gen
-
+	
         @@instance = Classement_gen.new
 	private_class_method :new
 
@@ -17,19 +17,22 @@ class Classement_gen
 		rescue ArgumentError => e
   			puts "Could not parse YAML: #{e.message}"
 		end
-		return tab if (tab.size <= 1)
-
-		swapped = true
-		while swapped do
-			swapped = false
-			0.upto(tab.size-2) do |i|
-				if tab[i].donneScore < tab[i+1].donneScore
-					tab[i], tab[i+1] = tab[i+1], tab[i]
-					swapped = true
+		puts tab.to_s
+		if(tab)
+			return tab if tab.size <= 1
+			swapped = true
+			while swapped do
+				swapped = false
+				0.upto(tab.size-2) do |i|
+					if tab[i].donneScore < tab[i+1].donneScore
+						tab[i], tab[i+1] = tab[i+1], tab[i]
+						swapped = true
+					end
 				end
 			end
+		  	tab
 		end
-	  	tab
+		
 	end
 
 
@@ -38,41 +41,50 @@ class Classement_gen
 	def ajoutable?(unJoueur)
 		res = Array.new
 		tab = deserealiseJoueurs
-		unMode = unJoueur.donneMode
-		tab.each do |joueur|
-			if joueur.donneMode == unMode
-				res.push(joueur)
+		if(tab != nil)
+			unMode = unJoueur.donneMode
+			tab.each do |joueur|
+				if joueur.donneMode == unMode
+					res.push(joueur)
+				end
+			
 			end
 
-		end
-
-		if res.size < 10
-			return true
-		else
-			if  res[res.size-1].donneScore < unJoueur.donneScore
+			if res.size < 10
 				return true
 			else
-				return false
+				if  res[res.size-1].donneScore < unJoueur.donneScore
+					return true
+				else
+					return false 
+				end
 			end
-		end
-
+		else 
+			return true 
+		end 
+		
 	end
 
 
 	def remplaceDernier(tab, unJoueur)
 		res = Array.new
-		tab.each do |joueur|
-			if joueur.donneMode == unJoueur.donneMode
-				res.push(joueur)
+		if(tab != nil)
+			tab.each do |joueur|
+				if joueur.donneMode == unJoueur.donneMode
+					res.push(joueur)
+				end
+			
 			end
+			if(res.size < 10)
+				res.push(unJoueur)
+			else
+			  	tab[tab.index(res[res.size-1])] = unJoueur
+			end
+		else 
+			res.push(unJoueur)
+		end 
 
-		end
-		if(res.size < 10)
-			tab.push(unJoueur)
-		else
-		  	tab[tab.index(res[res.size-1])] = unJoueur
-		end
-		return tab
+		return res
 	end
 
 	def ajouteJoueur(unJoueur)
@@ -80,7 +92,7 @@ class Classement_gen
 		if ajoutable?(unJoueur)
 			tab = remplaceDernier(tab, unJoueur)
 			File.open("./Classement/output.yml", "w") {|f| f.write(tab.to_yaml) }
-		end
+		end	
 	end
 
 end
