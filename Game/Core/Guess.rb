067a176f0e4fb4c7df1846ec26a399e853b6ef7e
Marvin <marvinmoisy@gmail.com>
2018-04-11 22:27:31 +0200

    Core commented

--- Game/Core/Guess.rb
+++ Game/Core/Guess.rb
@@ -1,27 +1,51 @@
 require File.dirname(__FILE__) + "/Moves"
 
+
+##
+# Representation of a Guess, a Guess is like a removable try
 class Guess
 	@prev
 	@grid
 	@moves
 
-	attr_accessor :prev,  :moves
-	attr_reader :grid
+	attr_accessor :prev #The previous Guess
+	attr_accessor :moves #The Moves of the Guess
+	attr_reader :grid #The Grid
 
+	##
+	# Creates a new Guess object
+	# * *Arguments* :
+	#   - +grid+     -> the Grid
+	#   - +prev+     -> the previous Guess if there is one
 	def initialize(grid, prev = nil)
 		@prev = prev
 		@grid = grid
 		@moves = Moves.new()
 	end
 
+	##
+	# Returns a boolean indicates if this Guess is the root one (i.e there no previous guess)
+	# * *Returns* :
+	#   - Boolean
 	def root?
 		@prev == nil
 	end
 
+	##
+	# Returns a new Guess who has this one as previous
+	# * *Returns* :
+	#   - Guess
 	def next
 		Guess.new(@grid.copyFrozen, self)
 	end
 
+	##
+	# Returns the Cell at the given row, col
+	# * *Arguments* :
+	#   - +row+     -> The row index of The wanted Cell
+	#   - +col+     -> The col index of The wanted Cell
+	# * *Returns* :
+	#   - The Cell
 	def cellAt(row, col)
 		@grid.cellAt(row, col)
 	end
@@ -38,10 +62,18 @@ class Guess
 		@grid.to_s
 	end
 
+	##
+	# Undo the last Move did in this Guess
+	# * *Arguments* :
+	#   - +game+     -> The game
 	def undo(game)
 		@moves.undo(game)
 	end
 
+	##
+	# Redo the last Move did in this Guess
+	# * *Arguments* :
+	#   - +game+     -> The game
 	def redo(game)
 		@moves.redo(game)
 	end
