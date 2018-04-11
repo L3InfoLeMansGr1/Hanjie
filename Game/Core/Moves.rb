1a5314e49d82674ac89055dfb38cae9115174c1d
Marvin <marvinmoisy@gmail.com>
2018-04-11 18:34:22 +0200

    Moves commented

--- Game/Core/Moves.rb
+++ Game/Core/Moves.rb
@@ -1,29 +1,47 @@
+##
+# Representation of a Move collection
+
+
 class Moves
-	@moves
-	@redo
+	@moves #All the Move s
+	@redo #Redo stack
 
-	attr_reader :moves
+	attr_reader :moves #Stack of Move s
 
+	##
+	# Creates a new Moves object
+	# * *Arguments* :
+	#   - +moves+ -> all the Move s (empty by default)
 	def initialize(moves = [])
 		@moves = moves
 		@redo = []
 	end
 
+	##
+	# Adds a Move to moves stack and clears redo stack
+	# * *Arguments* :
+	#   - +move+ -> the move to add
 	def add(move)
 		@moves << move
 		clearRedo
 	end
 
-	def serialize
-		data = @moves.map(&:serialize)
-	end
-
+	##
+	# Replay each Move on th given Game
+	# * *Arguments* :
+	#   - +game+ -> the game to replay on
 	def replay(game)
 		@moves.each { |move|
 			move.replay(game)
 		}
 	end
 
+	##
+	# undo the last Move (i.e replay it on the given game) and push it on redo stack
+	# * *Arguments* :
+	#   - +game+ -> the game to undo on
+	# * *Returns* :
+	#   - all undo cells
 	def undo(game)
 		if @moves.last != nil
 			@moves.last.replayWithoutAdd(game)
@@ -33,6 +51,12 @@ class Moves
 		return []
 	end
 
+	##
+	# redo the last undo Move (i.e replay it on the given game) and push it on undo stack
+	# * *Arguments* :
+	#   - +game+ -> the game to redo on
+	# * *Returns* :
+	#   - all redo cells
 	def redo(game)
 		if @redo.last != nil
 			@redo.last.replayWithoutAdd(game)
@@ -42,6 +66,8 @@ class Moves
 		return []
 	end
 
+	##
+	# Clears redo stack
 	def clearRedo
 		@redo.clear
 	end
